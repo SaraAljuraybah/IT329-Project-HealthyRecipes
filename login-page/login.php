8b444d23d9f03ca68f79d5b60d874922531b0ae3
SaraAljuraybah <Saraaljrebah1@gmail.com>
2026-03-31 11:21:10 +0300

    Add files via upload

--- login-page/login.php
+++ login-page/login.php
@@ -1,36 +1,46 @@
 <?php
 
 session_start();
-
-include "db.php";
+include "../db.php";
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-/* check user */
-
 $sql = "SELECT * FROM user WHERE emailAddress='$email'";
-
 $result = $conn->query($sql);
 
-if($result->num_rows > 0){
 
-    $user = $result->fetch_assoc();
-
-    if($password == $user['password']){
-
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['firstName'] = $user['firstName'];
-
-        header("Location: user.html");
-        exit();
-
-    }
+if($result->num_rows == 0){
+    echo "<script>
+    alert('Invalid Email or Password');
+    window.location.href='login.html';
+    </script>";
+    exit();
 }
 
-echo "<script>
-alert('Invalid Email or Password');
-window.location.href='login.html';
-</script>";
+$user = $result->fetch_assoc();
+
+
+if(!password_verify($password, $user['password'])){
+    echo "<script>
+    alert('Invalid Email or Password');
+    window.location.href='login.html';
+    </script>";
+    exit();
+}
+
+
+$_SESSION['user_id'] = $user['id'];
+$_SESSION['firstName'] = $user['firstName'];
+$_SESSION['user_type'] = $user['userType'];
+
+
+if($user['userType'] == "admin"){
+    header("Location: ../admin-page/admin.html");
+} else {
+    header("Location: ../user-page/user.html");
+}
+
+exit();
 
 ?>
